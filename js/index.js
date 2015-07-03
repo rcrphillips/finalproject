0188b634d98be63b5c6b324cd14d659a98645557
rcrphillips <robert.phillips@guardian.co.uk>
2015-07-03 16:23:36 +0100

    switched to module pattern for js

--- js/index.js
+++ js/index.js
@@ -1,48 +1,137 @@
-//Creates a new item from each result from the api response, 
-//The resultItem passed through is each 'result' which has been split up in the getJson function
-function createItem(resultItem){
-	//breaks each result down into elements array
-	var elements = resultItem.elements;
-	var thumbnail = "http://fallback.png ";
-	var trail = resultItem.fields.trailText;
-	var link = resultItem.webUrl;
-	console.log(link);
-	
-	resultItem.elements.forEach(function(element) {
-		element.assets.forEach(function(asset) {
-			if(asset.typeData.width === "620") {
-				thumbnail = asset.file;
-			}
+var TeamLoader = function () {
+
+	page = 1;
+	var teamName = "arsenal";
+	var mainColour = "red";
+	var secondaryColour = "white";
+	var template = $('#result-item').html();
+
+	function getTeam(newTeamName){
+
+		if (newTeamName) {
+			teamName = newTeamName;
+		}
+
+		return $.getJSON('http://content.guardianapis.com/search?tag=football%2f'+teamName+'%2Ctone%2Fmatchreports&show-fields=trailText&show-elements=image&page='+page+'&api-key=' + API_KEY, function(json) {
+			
+			//creates an object from the API query (json is overall response, response is the blob of json, results are all of the array of results from the response)
+			var results = json.response.results;	
+
+			// This splits each result from the results object above and outputs it as a 'resultItem'
+			results.forEach(function(resultItem) {
+				//from each result item, the function calls createArticle with each individual resultItem
+				createArticle(resultItem);
+
+			console.log(json);
+			});	
+
+		}).done( function(){
+			//on the completion of the json response, change the styles for each article header based on the team colours
+			$('#results h2').css({
+				color: secondaryColour,
+				backgroundColor: mainColour
+			});
 		});
-	});
-	//Appends the html to the end of the body. Need to add classes for styling
-	$('#results').append("<article><a href="+link+"><h2>" + resultItem.webTitle + "</h2>"+"<img src="+thumbnail+"><p>"+trail+"</p></a></article>");
-	//Handlebars JS?
+	};
+
+	function findImagesInResults(resultItem) {
+		resultItem.elements.forEach(function(element) {
+		
+			var biggestImageIndex;
+			var biggestImageWidth = 0;
+
+			element.assets.forEach(function(asset, index) {
+				if(asset.typeData.width > biggestImageWidth) {
+					biggestImageWidth = asset.typeData.width;
+					biggestImageIndex = index;
+				} 
+			});
+
+			resultItem.thumbnail = element.assets[biggestImageIndex].file;
+		});
+
+		return resultItem;
+	}
+
+	//The resultItem passed through is each 'result' which has been split up in the getJson function
+	function createArticle(resultItem, main){
+		//breaks each result down into elements array
+		var resultItem = findImagesInResults(resultItem);
+
+		$('#results').append(Mustache.to_html(template, resultItem));
+	};
+
+	function loadMore(){
+		page++;
+		return getTeam();
+
+	}
+
+	function setColours (main, secondary) {
+		mainColour = main;
+		secondaryColour = secondary;
+	}
+
+	function resetPage (page) {
+		page = page;
+	}
+
+	return {
+		getTeam: getTeam,
+		setColours: setColours,
+		loadMore: loadMore,
+		resetPage: resetPage
+	};
+
 };
 
 
-//Gets the id value from the clicked on image - Will be used to populate the api query 
-$('li img').click(function(event) {
-	$('#results').empty();
-	var teamName = this.id;
-	getTeam(teamName);
+$(document).ready(function() {
+
+	var teamLoader = new TeamLoader();
+
+	var waypoint = new Waypoint({
+		element: $('#bottom'),
+		handler: function(direction) {
+	  		if (direction === 'down') {
+	  			console.log('triggered');
+	  			teamLoader.loadMore().done(function () {
+	  				Waypoint.refreshAll();
+	  			});
+	  		};	
+
+		},
+		offset: 'bottom-in-view'
+	});
+
+	//Function to get the  
+	$('li img').click(function(event) {
+		//empty the results div before each team is clicked on
+		$('#results').empty();
+
+		//rests page variable when team is clicked on
+		teamLoader.resetPage(
+			page = 1
+		);
+
+
+		//make the teams active/inactive when clicked on
+		$('li img').addClass('inactive');
+		$('li img').removeClass('active');
+		$(this).addClass('active');
+
+		//gets the colour attributes from the clicked on image for setting result colours
+		teamLoader.setColours(
+			$(this).data('main'), 
+			$(this).data('secondary')
+		);
+
+		//gets the id from the clicked on image and passes through to API query
+		//calls the getTeam function below with the arguments from the clicked team
+		teamLoader.getTeam(this.id).done(function () {
+			Waypoint.refreshAll();
+		});
+	});
+
 });
-
-//wrap below in a function that accepts argument of teamName
-
-function getTeam(teamName){
-	$.getJSON('http://content.guardianapis.com/search?tag=football%2f'+teamName+'%2Ctone%2Fmatchreports&show-fields=trailText&show-elements=image&page=1&api-key=' + API_KEY, function(json) {
-		/*optional stuff to do after success */
-		var results = json.response.results;
-				
-		// This is a function which takes each result from the api and response and outputs it as a 'resultItem'
-		results.forEach(function(resultItem) {
-			// console.log('resultItem', resultItem);
-			createItem(resultItem);
-		});
-
-	});
-};
-
-$('h1').click(getTeam);
 
